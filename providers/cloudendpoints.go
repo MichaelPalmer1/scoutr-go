9d625d729f904b08ee2019f46cf80c04ca20f230
Michael Palmer <MichaelPalmer1@users.noreply.github.com>
2020-02-17 22:58:07 -0500

    playing with cloud endpoints

---
+++ providers/cloudendpoints.go
@@ -0,0 +1,37 @@
+package providers
+
+import (
+	"net/http"
+
+	"github.com/MichaelPalmer1/simple-api-go/models"
+)
+
+// TODO: Add helper functions for Google Cloud Endpoints
+
+// InitCloudEndpoint : Initialize a Google Cloud Endpoint
+func InitCloudEndpoint(r *http.Request) models.Request {
+	// Build request user
+	requestUser := models.RequestUser{
+		// ID: event.RequestContext.Identity.APIKeyID,
+	}
+
+	// Parse query params
+	queryParams := make(map[string]string)
+	for key, values := range req.URL.Query() {
+		queryParams[key] = values[0]
+	}
+
+	// Create the request
+	request := models.Request{
+		Method: r.Method,
+		Path:   r.URL.Path,
+		Body:   r.Body,
+		// PathParams:  event.PathParameters,
+		QueryParams: queryParams,
+		UserAgent:   r.UserAgent(),
+		SourceIP:    r.RemoteAddr,
+		User:        requestUser,
+	}
+
+	return request
+}
